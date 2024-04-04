51a7431cacba6d1b1be6020c3b9b252ac5e8a2ba
Karl Marx Roxas - NAMRIA Office <karlm.roxas@gmail.com>
2024-04-04 09:04:50 +0800

    add setstatus UI

---
+++ src/app/setstatus/setstatus.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-setstatus',
+  templateUrl: './setstatus.component.html',
+  styleUrls: ['./setstatus.component.css']
+})
+export class SetstatusComponent {
+
+}
