bc4385dc9da467cfbdb3ee09f1a6851ed39b17b2
Karl Marx Roxas - NAMRIA Pavilion SSD <karlm.roxas@gmail.com>
2024-06-10 15:57:32 +0800

    fixed bug in timezone, time filter, tried to implement excel formattin but failed

--- src/app/admin/monthly-attendance/monthly-attendance.component.ts
+++ src/app/admin/monthly-attendance/monthly-attendance.component.ts
@@ -7,156 +7,196 @@ import { FilterMatchMode, PrimeNGConfig } from 'primeng/api';
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
 @Component({
-  selector: 'app-monthly-attendance',
-  templateUrl: './monthly-attendance.component.html',
-  styleUrls: ['./monthly-attendance.component.css'],
+    selector: 'app-monthly-attendance',
+    templateUrl: './monthly-attendance.component.html',
+    styleUrls: ['./monthly-attendance.component.css'],
 })
 export class MonthlyAttendanceComponent implements OnInit {
-  @ViewChild('dt') dt!: Table;
+    @ViewChild('dt') dt!: Table;
 
-  employeeAttendance!: EmployeeAttendance[];
+    employeeAttendance!: EmployeeAttendance[];
 
-  selectedEmployee!: EmployeeAttendance;
+    selectedEmployee!: EmployeeAttendance;
 
-  employeeFirstNames!: any[];
+    employeeFirstNames!: any[];
 
-  employeeLastNames!: any[];
+    employeeLastNames!: any[];
 
-  rangeDates!: Date[];
+    rangeDates!: Date[];
 
-  cols!: any[];
+    cols!: any[];
 
-  exportColumns!: any[];
+    exportColumns!: any[];
 
-  loading: boolean = true;
+    loading: boolean = true;
 
-  constructor(
-    private datePipe: DatePipe,
-    private employeeAttendanceService: EmployeeAttendanceService,
-    private primengConfig: PrimeNGConfig,
-  ) {}
+    constructor(
+        private datePipe: DatePipe,
+        private employeeAttendanceService: EmployeeAttendanceService,
+        private primengConfig: PrimeNGConfig,
+    ) { }
 
-  ngOnInit() {
-    this.primengConfig.filterMatchModeOptions = {
-      text: [
-        FilterMatchMode.STARTS_WITH,
-        FilterMatchMode.CONTAINS,
-        FilterMatchMode.NOT_CONTAINS,
-        FilterMatchMode.ENDS_WITH,
-        FilterMatchMode.EQUALS,
-        FilterMatchMode.NOT_EQUALS,
-      ],
-      numeric: [
-        FilterMatchMode.EQUALS,
-        FilterMatchMode.NOT_EQUALS,
-        FilterMatchMode.LESS_THAN,
-        FilterMatchMode.LESS_THAN_OR_EQUAL_TO,
-        FilterMatchMode.GREATER_THAN,
-        FilterMatchMode.GREATER_THAN_OR_EQUAL_TO,
-      ],
-      date: [
-        FilterMatchMode.DATE_IS,
-        FilterMatchMode.DATE_IS_NOT,
-        FilterMatchMode.DATE_BEFORE,
-        FilterMatchMode.DATE_AFTER,
-      ],
-    };
+    ngOnInit() {
+        this.primengConfig.filterMatchModeOptions = {
+            text: [
+                FilterMatchMode.STARTS_WITH,
+                FilterMatchMode.CONTAINS,
+                FilterMatchMode.NOT_CONTAINS,
+                FilterMatchMode.ENDS_WITH,
+                FilterMatchMode.EQUALS,
+                FilterMatchMode.NOT_EQUALS,
+            ],
+            numeric: [
+                FilterMatchMode.EQUALS,
+                FilterMatchMode.NOT_EQUALS,
+                FilterMatchMode.LESS_THAN,
+                FilterMatchMode.LESS_THAN_OR_EQUAL_TO,
+                FilterMatchMode.GREATER_THAN,
+                FilterMatchMode.GREATER_THAN_OR_EQUAL_TO,
+            ],
+            date: [
+                FilterMatchMode.DATE_IS,
+                FilterMatchMode.DATE_IS_NOT,
+                FilterMatchMode.DATE_BEFORE,
+                FilterMatchMode.DATE_AFTER,
+            ],
+        };
 
-    this.employeeAttendanceService.getEmployeeAttendanceData().subscribe((data) => {
-      this.loading = false;
-      this.employeeAttendance = data;
-      console.log("All Attendance Data: ",data)
+       
+        
+        this.employeeAttendanceService.getEmployeeAttendanceData().subscribe((data) => {
+            this.loading = false;
+            this.employeeAttendance = data.map((data: any) => {
+                if(data.time_in != null){
+                    let time_in = new Date(data.time_in);
+                    data.time_in = time_in;
+                }
 
-      this.employeeFirstNames = this.employeeAttendance.map((item) => item.first_name);
-      this.employeeLastNames = this.employeeAttendance.map((item) => item.last_name);
+                if(data.time_out != null){
+                    let time_out = new Date(data.time_out);
+                    data.time_out = time_out;
+                }
+                return data;
+            });
+        
+            this.employeeFirstNames = this.employeeAttendance.map((item) => item.first_name);
+            this.employeeLastNames = this.employeeAttendance.map((item) => item.last_name);
+        });
 
-    });
+        this.cols = [
+            { field: 'first_name', header: 'First Name' },
+            { field: 'last_name', header: 'Last Name' },
+            { field: 'time_in', header: 'Time in' },
+            { field: 'time_out', header: 'Time out' },
+        ];
 
-    this.cols = [
-      { field: 'first_name', header: 'First Name' },
-      { field: 'last_name', header: 'Last Name' },
-      { field: 'time_in', header: 'Time in' },
-      { field: 'time_out', header: 'Time out' },
-    ];
+        this.exportColumns = this.cols.map((col) => ({
+            title: col.header,
+            dataKey: col.field,
+        }));
+    }
 
-    this.exportColumns = this.cols.map((col) => ({
-      title: col.header,
-      dataKey: col.field,
-    }));
-  }
+    date() {
+        let jsonDateRange = {
+            startDate: this.datePipe.transform(this.rangeDates[0], 'MM/dd/yy'),
+            endDate: this.datePipe.transform(this.rangeDates[1], 'MM/dd/yyyy'),
+        };
 
-  date() {
-    let jsonDateRange = {
-      startDate: this.datePipe.transform(this.rangeDates[0], 'MM/dd/yy'),
-      endDate: this.datePipe.transform(this.rangeDates[1], 'MM/dd/yyyy'),
-    };
+        console.log(jsonDateRange);
+    }
 
-    console.log(jsonDateRange);
-  }
+    applyFilterGlobal($event: any, stringVal: any) {
+        this.dt.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
+    }
 
-  applyFilterGlobal($event: any, stringVal: any) {
-    this.dt.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
-  }
+    clear(table: Table) {
+        table.clear();
+    }
 
-  clear(table: Table) {
-    table.clear();
-  }
+    exportExcel() {
+        const data = this.employeeAttendance.map((item) => {
 
-  exportExcel(){
-    const data = this.employeeAttendance.map((item) => ({
-                Id: item.Id,
-                Name: item.first_name + " " + item.last_name,
-                time_in: this.convertToManilaTime(item.time_in), // Convert time to ISOstring
-                time_out: item.time_out
-              }));
-    console.log(data);
+            // if(item.time_in != null){
+            //     let date = new Date(item.time_in);
+            //     let isoString = date.toISOString();
+            //     item.time_in = `Time In: ${isoString.replace('T', ' ').substring(0, 16)}`;
+            // }
 
-  }
+            // if(item.time_out != null){
+            //     let date = new Date(item.time_out);
+            //     let isoString = date.toISOString();
+            //     item.time_out = `Time In: ${isoString.replace('T', ' ').substring(0, 16)}`;
+            // }
+            return item
+        });
+        console.log(this.dt);
 
-  convertToManilaTime(isoString: Date){
-    const date = new Date(isoString);
-    const timezoneOffset = date.getTimezoneOffset() * 60000; // Convert offset to milliseconds
-    const manilaOffset = 8 * 60 * 60000; // Manila offset in milliseconds (UTC+8)
-    const manilaTime = new Date(date.getTime() + timezoneOffset + manilaOffset);
+    
+        const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.dt.el.nativeElement, 
+            { dateNF: 'yyyy-mm-dd hh:mm', cellDates: true });
 
-    return manilaTime.toISOString();
-  }
+            let columnLetters = Object.keys(ws).reduce((letters, key) => {
+                const letterMatch = key.match(/[A-Z]+/);
+                if (letterMatch) {
+                  letters.add(letterMatch[0]);
+                }
+                return letters;
+              }, new Set());
+              
+              // Set the width for each column
+              ws['!cols'] = Array.from(columnLetters).map(() => ({ wch: 15 }));
+
+        const wb: XLSX.WorkBook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    
+        XLSX.writeFile(wb, 'SheetJS.xlsx');
+
+    }
+
+    convertToManilaTime(isoString: Date) {
+        const date = new Date(isoString);
+        const timezoneOffset = date.getTimezoneOffset() * 60000; // Convert offset to milliseconds
+        const manilaOffset = 8 * 60 * 60000; // Manila offset in milliseconds (UTC+8)
+        const manilaTime = new Date(date.getTime() + timezoneOffset + manilaOffset);
+
+        return manilaTime.toISOString();
+    }
 
 
-//   Old export
-//   exportExcel() {
-//     import('xlsx').then((xlsx) => {
-//       const data = this.employeeAttendance.map((item) => ({
-//         ...item,
-//         time_in: item.time_in.toLocaleString('en-US', {
-//           timeZone: 'Asia/Manila',
-//         }),
-//         time_out: item.time_out.toLocaleString('en-US', {
-//           timeZone: 'Asia/Manila',
-//         }),
-//       }));
-//       const worksheet = xlsx.utils.json_to_sheet(data);
-//       const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
-//       const excelBuffer: any = xlsx.write(workbook, {
-//         bookType: 'xlsx',
-//         type: 'array',
-//       });
-//       this.saveAsExcelFile(excelBuffer, 'employee-attendance');
-//     });
-//   }
+    //   Old export
+    //   exportExcel() {
+    //     import('xlsx').then((xlsx) => {
+    //       const data = this.employeeAttendance.map((item) => ({
+    //         ...item,
+    //         time_in: item.time_in.toLocaleString('en-US', {
+    //           timeZone: 'Asia/Manila',
+    //         }),
+    //         time_out: item.time_out.toLocaleString('en-US', {
+    //           timeZone: 'Asia/Manila',
+    //         }),
+    //       }));
+    //       const worksheet = xlsx.utils.json_to_sheet(data);
+    //       const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+    //       const excelBuffer: any = xlsx.write(workbook, {
+    //         bookType: 'xlsx',
+    //         type: 'array',
+    //       });
+    //       this.saveAsExcelFile(excelBuffer, 'employee-attendance');
+    //     });
+    //   }
 
-  saveAsExcelFile(buffer: any, fileName: string): void {
-    let EXCEL_TYPE =
-      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-    let EXCEL_EXTENSION = '.xlsx';
-    const data: Blob = new Blob([buffer], {
-      type: EXCEL_TYPE,
-    });
-    FileSaver.saveAs(
-      data,
-      fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION,
-    );
-  }
+    saveAsExcelFile(buffer: any, fileName: string): void {
+        let EXCEL_TYPE =
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+        let EXCEL_EXTENSION = '.xlsx';
+        const data: Blob = new Blob([buffer], {
+            type: EXCEL_TYPE,
+        });
+        FileSaver.saveAs(
+            data,
+            fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION,
+        );
+    }
 
- 
+
 }
